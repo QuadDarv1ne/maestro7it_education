736fdcf70c2cf23c73e725c2a5c961f05d166d8e
Max Dupley <51045274+QuadDarv1ne@users.noreply.github.com>
2024-12-11 20:09:34 +0300

    Update index.cpp

--- c_plus_plus/Lesson_1/index.cpp
+++ c_plus_plus/Lesson_1/index.cpp
@@ -1,10 +1,15 @@
 /*
-    В C++ iostream, fstream и ostream — это важные компоненты для работы с вводом/выводом
+    В C++ iostream, fstream и ostream — это важные компоненты для работы с вводом/выводом.
+    input <- входная информация
+    output <- выходная информация
+    file <- работа с файлами
 */
 
 #include <iostream> // <= подключение библиотеки для работы с вводом/выводом
 #include <fstream>  // <= подключение библиотеки для работы с файлами
 #include <ostream>  // <= подключение библиотеки для работы с выводом
+
+using namespace std;
 
 int main() {
     std::cout << "Hello, World" << std::endl;
