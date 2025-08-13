6dc8176f15af2c2d743cda51b23c8e2ed1da3f68
Maxim Dupley <maksimqwe42@mail.ru>
2025-08-13 19:38:11 +0300

    new file:   leetcode/Daily Question/cpp/326. Power of Three.cpp 	new file:   leetcode/Daily Question/csharp/326. Power of Three.cs 	new file:   leetcode/Daily Question/java/326. Power of Three.java 	new file:   leetcode/Daily Question/python/326. Power of Three.py 	new file:   leetcode/Daily Question/python3/326. Power of Three.py

---
+++ leetcode/Daily Question/python/326. Power of Three.py
@@ -0,0 +1,39 @@
+'''
+326. Power of Three (Python) by Dupley Maxim Igorevich
+https://leetcode.com/problems/power-of-three/description/?envType=daily-question&envId=2025-08-13
+'''
+
+class Solution:
+    def isPowerOfThree(self, n):
+        """
+        Определяет, является ли число n степенью числа 3.
+
+        Алгоритм:
+        - Максимальная степень числа 3, которая помещается в 32-битное целое число, равна 3^19 = 1162261467.
+        - Если n > 0 и 1162261467 делится на n без остатка, значит n является степенью числа 3.
+        - Преимущество данного метода в том, что он не использует циклы и логарифмы, а работает за O(1).
+
+        Параметры:
+        n (int): Целое число для проверки.
+
+        Возвращает:
+        bool: True, если n является степенью числа 3, иначе False.
+
+        Примеры:
+        >>> Solution().isPowerOfThree(27)
+        True
+        >>> Solution().isPowerOfThree(0)
+        False
+        >>> Solution().isPowerOfThree(9)
+        True
+        """
+        return n > 0 and 1162261467 % n == 0
+
+''' Полезные ссылки: '''
+# 1. 💠Telegram💠❃ Хижина программиста Æ: https://t.me/hut_programmer_07
+# 2. 💠Telegram №1💠 @quadd4rv1n7
+# 3. 💠Telegram №2💠 @dupley_maxim_1999
+# 4. Rutube канал: https://rutube.ru/channel/4218729/
+# 5. Plvideo канал: https://plvideo.ru/channel/AUPv_p1r5AQJ
+# 6. YouTube канал: https://www.youtube.com/@it-coders
+# 7. ВК группа: https://vk.com/science_geeks
