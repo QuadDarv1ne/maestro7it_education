98fff18ce00ee4df3a82ee374ecb6098d7a64467
QuadDarv1ne <maksimqwe42@mail.ru>
2025-09-25 16:22:45 +0300

    modified:   leetcode/Medium/cpp/120. Triangle.cpp 	modified:   leetcode/Medium/csharp/120. Triangle.cs 	modified:   leetcode/Medium/java/120. Triangle.java 	new file:   leetcode/Medium/javascript/120. Triangle.js

--- leetcode/Medium/cpp/120. Triangle.cpp
+++ leetcode/Medium/cpp/120. Triangle.cpp
@@ -29,9 +29,9 @@ public:
 
 /*
 ''' Полезные ссылки: '''
-# 1. 💠Telegram💠❃ Хижина программиста Æ: https://t.me/hut_programmer_07
-# 2. 💠Telegram №1💠 @quadd4rv1n7
-# 3. 💠Telegram №2💠 @dupley_maxim_1999
+# 1. Telegram ❃ Хижина программиста Æ: https://t.me/hut_programmer_07
+# 2. Telegram №1 @quadd4rv1n7
+# 3. Telegram №2 @dupley_maxim_1999
 # 4. Rutube канал: https://rutube.ru/channel/4218729/
 # 5. Plvideo канал: https://plvideo.ru/channel/AUPv_p1r5AQJ
 # 6. YouTube канал: https://www.youtube.com/@it-coders
