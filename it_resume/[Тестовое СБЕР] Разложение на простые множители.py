19617553914d798ef4982f15b97bc26d7b5b4291
Max Dupley <maksimqwe42@mail.ru>
2025-09-30 22:35:08 +0300

    modified:   "it_resume/[\320\242\320\265\321\201\321\202\320\276\320\262\320\276\320\265 \320\241\320\221\320\225\320\240] \320\240\320\260\320\267\320\273\320\276\320\266\320\265\320\275\320\270\320\265 \320\275\320\260 \320\277\321\200\320\276\321\201\321\202\321\213\320\265 \320\274\320\275\320\276\320\266\320\270\321\202\320\265\320\273\320\270.py"

--- it_resume/[Тестовое СБЕР] Разложение на простые множители.py
+++ it_resume/[Тестовое СБЕР] Разложение на простые множители.py
@@ -1,8 +1,24 @@
 '''
-https://itresume.ru/problems/sber-prime-factorization
+Решение задачи: https://itresume.ru/problems/sber-prime-factorization
 '''
 
 class Answer:
+    """
+    Автор: Дуплей Максим Игоревич
+    ORCID: https://orcid.org/0009-0007-7605-539X
+    GitHub: https://github.com/QuadDarv1ne/
+
+    Класс Answer содержит метод decomp, который разлагает факториал числа n
+    на простые множители и возвращает строковое представление в формате:
+    "a^b * c^d * ... * e", где a, c, ... — простые числа в порядке возрастания,
+    а b, d, ... — соответствующие показатели степени. Если показатель степени
+    равен 1, он не указывается.
+
+    Методы:
+    - decomp(n): возвращает строку разложения факториала n на простые множители.
+    - sieve_of_eratosthenes(n): возвращает список всех простых чисел до n включительно.
+    """
+
     def decomp(self, n):
         primes = self.sieve_of_eratosthenes(n)
         result = []
@@ -28,11 +44,13 @@ class Answer:
                     sieve[i] = False
         return [num for num, is_prime in enumerate(sieve) if is_prime]
 
-''' Полезные ссылки: '''
-# 1. Telegram ❃ Хижина программиста Æ: https://t.me/hut_programmer_07
-# 2. Telegram №1 @quadd4rv1n7
-# 3. Telegram №2 @dupley_maxim_1999
-# 4. Rutube канал: https://rutube.ru/channel/4218729/
-# 5. Plvideo канал: https://plvideo.ru/channel/AUPv_p1r5AQJ
-# 6. YouTube канал: https://www.youtube.com/@it-coders
-# 7. ВК группа: https://vk.com/science_geeks
+'''
+Полезные ссылки:
+1. Telegram ❃ Хижина программиста Æ: https://t.me/hut_programmer_07
+2. Telegram №1 @quadd4rv1n7
+3. Telegram №2 @dupley_maxim_1999
+4. Rutube канал: https://rutube.ru/channel/4218729/
+5. Plvideo канал: https://plvideo.ru/channel/AUPv_p1r5AQJ
+6. YouTube канал: https://www.youtube.com/@it-coders
+7. ВК группа: https://vk.com/science_geeks
+'''
