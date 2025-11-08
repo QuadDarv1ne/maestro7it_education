5d2a9e1a8970cbf4c87ecda98a520ad8876d1a03
QuadDarv1ne <maksimqwe42@mail.ru>
2025-11-08 20:34:08 +0300

    deleted:    python/solution_tasks/simple_hr/check_employees.py 	deleted:    python/solution_tasks/simple_hr/detailed_test.py 	deleted:    python/solution_tasks/simple_hr/form_test.html 	modified:   "python/solution_tasks/simple_hr/\320\267\320\260\320\264\320\260\320\275\320\270\320\265\320\223\321\203\321\202\320\275\320\270\320\272.pdf"

--- python/solution_tasks/simple_hr/check_employees.py
+++
@@ -1,9 +0,0 @@
-from app import create_app, db
-from app.models import Employee
-
-app = create_app()
-with app.app_context():
-    print(f'Number of employees: {Employee.query.count()}')
-    employees = Employee.query.limit(5).all()
-    for emp in employees:
-        print(f'{emp.id}: {emp.full_name} ({emp.email})')
