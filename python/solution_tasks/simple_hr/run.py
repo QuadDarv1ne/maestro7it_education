8bb94001932eec1a031e8e85f13e6971894254e8
QuadDarv1ne <maksimqwe42@mail.ru>
2025-11-16 19:14:19 +0300

    modified:   python/solution_tasks/simple_hr/run.py

--- python/solution_tasks/simple_hr/run.py
+++ python/solution_tasks/simple_hr/run.py
@@ -1,6 +1,6 @@
-from app import create_app
+from app import create_app, socketio
 
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(debug=True, host='127.0.0.1', port=5000)
+    socketio.run(app, debug=True, host='127.0.0.1', port=5000)
