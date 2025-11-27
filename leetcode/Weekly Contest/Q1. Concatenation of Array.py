f8b6b7a945ad747e8b12e78fb9efe68bb01f732a
QuadDarv1ne <maksimqwe42@mail.ru>
2025-11-27 17:19:00 +0300

    new file:   leetcode/Weekly Contest/Q1. Concatenation of Array.py

---
+++ leetcode/Weekly Contest/Q1. Concatenation of Array.py
@@ -0,0 +1,36 @@
+"""
+https://leetcode.com/problems/concatenation-of-array/description/?envType=problem-list-v2&envId=dsa-linear-shoal-array-i
+Автор: Дуплей Максим Игоревич
+ORCID: https://orcid.org/0009-0007-7605-539X
+GitHub: https://github.com/QuadDarv1ne/
+"""
+
+class Solution:
+    def getConcatenation(self, nums):
+        """
+        Конкатенация массива - оптимальное решение.
+        
+        Args:
+            nums: Исходный массив целых чисел
+            
+        Returns:
+            Новый массив, содержащий две копии исходного массива
+        """
+        n = len(nums)
+        result = [0] * (2 * n)  # Создаем массив нужного размера один раз
+        
+        # Копируем элементы в обе половины результирующего массива
+        for i in range(n):
+            result[i] = nums[i]        # Первая копия
+            result[i + n] = nums[i]    # Вторая копия
+            
+        return result
+
+''' Полезные ссылки: '''
+# 1. Telegram ❃ Хижина программиста Æ: https://t.me/hut_programmer_07
+# 2. Telegram №1 @quadd4rv1n7
+# 3. Telegram №2 @dupley_maxim_1999
+# 4. Rutube канал: https://rutube.ru/channel/4218729/
+# 5. Plvideo канал: https://plvideo.ru/channel/AUPv_p1r5AQJ
+# 6. YouTube канал: https://www.youtube.com/@it-coders
+# 7. ВК группа: https://vk.com/science_geeks
