a6b0901288f92763f9d9700bf96902dbd98f8203
QuadDarv1ne <maksimqwe42@mail.ru>
2025-11-30 13:15:23 +0300

    new file:   leetcode/Medium/cpp/50. Pow(x, n).cpp 	new file:   leetcode/Medium/csharp/50. Pow(x, n).cs 	new file:   leetcode/Medium/java/50. Pow(x, n).java 	new file:   leetcode/Medium/javascript/50. Pow(x, n).js 	new file:   leetcode/Medium/python/50. Pow(x, n).py

---
+++ leetcode/Medium/csharp/50. Pow(x, n).cs
@@ -0,0 +1,40 @@
+/**
+ * https://leetcode.com/problems/powx-n/description/
+ * Автор: Дуплей Максим Игоревич - AGLA
+ * ORCID: https://orcid.org/0009-0007-7605-539X
+ * GitHub: https://github.com/QuadDarv1ne/
+ * 
+ * Решение задачи "Pow(x, n)" на C#
+ * 
+ * Полезные ссылки:
+ * 1. Telegram ❃ Хижина программиста Æ: https://t.me/hut_programmer_07
+ * 2. Telegram №1 @quadd4rv1n7
+ * 3. Telegram №2 @dupley_maxim_1999
+ * 4. Rutube канал: https://rutube.ru/channel/4218729/
+ * 5. Plvideo канал: https://plvideo.ru/channel/AUPv_p1r5AQJ
+ * 6. YouTube канал: https://www.youtube.com/@it-coders
+ * 7. ВК группа: https://vk.com/science_geeks
+ */
+
+public class Solution {
+    public double MyPow(double x, int n) {
+        long N = n;
+        if (N < 0) {
+            x = 1 / x;
+            N = -N;
+        }
+        
+        double result = 1.0;
+        double current = x;
+        
+        while (N > 0) {
+            if (N % 2 == 1) {
+                result *= current;
+            }
+            current *= current;
+            N /= 2;
+        }
+        
+        return result;
+    }
+}
---
+++ leetcode/Medium/python/50. Pow(x, n).py
@@ -0,0 +1,47 @@
+'''
+https://leetcode.com/problems/powx-n/description/
+Автор: Дуплей Максим Игоревич - AGLA
+ORCID: https://orcid.org/0009-0007-7605-539X
+GitHub: https://github.com/QuadDarv1ne/
+
+Решение задачи "Pow(x, n)"
+
+Полезные ссылки:
+1. Telegram ❃ Хижина программиста Æ: https://t.me/hut_programmer_07
+2. Telegram №1 @quadd4rv1n7
+3. Telegram №2 @dupley_maxim_1999
+4. Rutube канал: https://rutube.ru/channel/4218729/
+5. Plvideo канал: https://plvideo.ru/channel/AUPv_p1r5AQJ
+6. YouTube канал: https://www.youtube.com/@it-coders
+7. ВК группа: https://vk.com/science_geeks
+'''
+
+class Solution(object):
+    def myPow(self, x, n):
+        """
+        :type x: float
+        :type n: int
+        :rtype: float
+        """
+        # Базовый случай
+        if n == 0:
+            return 1.0
+        
+        # Обработка отрицательной степени
+        if n < 0:
+            x = 1 / x
+            n = -n
+        
+        result = 1.0
+        current_product = x
+        
+        while n > 0:
+            # Если n нечетное, умножаем результат на текущее значение x
+            if n % 2 == 1:
+                result *= current_product
+            # Возводим x в квадрат
+            current_product *= current_product
+            # Делим n на 2
+            n //= 2
+        
+        return result
