03c26679f248eeae5d623a65b6c1205bce6125dd
QuadDarv1ne <maksimqwe42@mail.ru>
2025-12-07 14:59:10 +0300

    new file:   leetcode/Medium/cpp/61. Rotate List.cpp 	new file:   leetcode/Medium/csharp/61. Rotate List.cs 	new file:   leetcode/Medium/java/61. Rotate List.java 	new file:   leetcode/Medium/javascript/61. Rotate List.js 	new file:   leetcode/Medium/python/61. Rotate List.py

---
+++ leetcode/Medium/java/61. Rotate List.java
@@ -0,0 +1,61 @@
+class Solution {
+    public ListNode rotateRight(ListNode head, int k) {
+        /**
+         * Автор: Дуплей Максим Игоревич
+         * ORCID: https://orcid.org/0009-0007-7605-539X
+         * GitHub: https://github.com/QuadDarv1ne/
+         * 
+         * Задача: Rotate List (LeetCode)
+         * Алгоритм: Вращение связного списка вправо на k позиций
+         * Сложность: O(n) по времени, O(1) по памяти
+         * 
+         * Идея решения:
+         * 1. Находим длину списка и последний узел
+         * 2. Нормализуем k через модуль (k % length)
+         * 3. Находим новый хвост на позиции (length - k - 1)
+         * 4. Переподключаем узлы: новая голова, разрыв и соединение
+         */
+        
+        // Обработка граничных случаев
+        if (head == null || head.next == null || k == 0) {
+            return head;
+        }
+        
+        // Шаг 1: Найти длину списка и последний узел
+        int length = 1;
+        ListNode last = head;
+        while (last.next != null) {
+            last = last.next;
+            length++;
+        }
+        
+        // Шаг 2: Нормализовать k (обработать случай k > length)
+        k = k % length;
+        if (k == 0) {
+            return head; // Вращение не требуется
+        }
+        
+        // Шаг 3: Найти новый хвост (на позиции length - k - 1)
+        ListNode newTail = head;
+        for (int i = 0; i < length - k - 1; i++) {
+            newTail = newTail.next;
+        }
+        
+        // Шаг 4: Установить новую голову и переподключить
+        ListNode newHead = newTail.next;
+        newTail.next = null;      // Разорвать связь
+        last.next = head;         // Соединить старый хвост со старой головой
+        
+        return newHead;
+    }
+}
+
+/*
+ * Полезные ссылки автора:
+ * Telegram ❃ Хижина программиста Æ: https://t.me/hut_programmer_07
+ * Telegram: @quadd4rv1n7, @dupley_maxim_1999
+ * Rutube: https://rutube.ru/channel/4218729/
+ * Plvideo: https://plvideo.ru/channel/AUPv_p1r5AQJ
+ * YouTube: https://www.youtube.com/@it-coders
+ * VK: https://vk.com/science_geeks
+ */
---
+++ leetcode/Medium/python/61. Rotate List.py
@@ -0,0 +1,55 @@
+class Solution:
+    def rotateRight(self, head, k):
+        """
+        Автор: Дуплей Максим Игоревич
+        ORCID: https://orcid.org/0009-0007-7605-539X
+        GitHub: https://github.com/QuadDarv1ne/
+        
+        Задача: Rotate List (LeetCode)
+        Алгоритм: Вращение связного списка вправо на k позиций
+        Сложность: O(n) по времени, O(1) по памяти
+        
+        Идея решения:
+        1. Находим длину списка и последний узел
+        2. Нормализуем k через модуль (k % length)
+        3. Находим новый хвост на позиции (length - k - 1)
+        4. Переподключаем узлы: новая голова, разрыв и соединение
+        """
+        
+        # Обработка граничных случаев
+        if not head or not head.next or k == 0:
+            return head
+        
+        # Шаг 1: Найти длину списка и последний узел
+        length = 1
+        last = head
+        while last.next:
+            last = last.next
+            length += 1
+        
+        # Шаг 2: Нормализовать k (обработать случай k > length)
+        k = k % length
+        if k == 0:
+            return head  # Вращение не требуется
+        
+        # Шаг 3: Найти новый хвост (на позиции length - k - 1)
+        new_tail = head
+        for _ in range(length - k - 1):
+            new_tail = new_tail.next
+        
+        # Шаг 4: Установить новую голову и переподключить
+        new_head = new_tail.next
+        new_tail.next = None      # Разорвать связь
+        last.next = head          # Соединить старый хвост со старой головой
+        
+        return new_head
+
+"""
+Полезные ссылки автора:
+Telegram ❃ Хижина программиста Æ: https://t.me/hut_programmer_07
+Telegram: @quadd4rv1n7, @dupley_maxim_1999
+Rutube: https://rutube.ru/channel/4218729/
+Plvideo: https://plvideo.ru/channel/AUPv_p1r5AQJ
+YouTube: https://www.youtube.com/@it-coders
+VK: https://vk.com/science_geeks
+"""
