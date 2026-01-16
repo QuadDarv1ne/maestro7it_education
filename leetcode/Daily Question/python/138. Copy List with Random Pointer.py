a823f3ee793c31628052bd408eef8339da0f7def
QuadDarv1ne <maksimqwe42@mail.ru>
2026-01-16 17:11:52 +0300

    modified:   leetcode/Daily Question/cpp/138. Copy List with Random Pointer.cpp 	modified:   leetcode/Daily Question/csharp/138. Copy List with Random Pointer.cs 	modified:   leetcode/Daily Question/java/138. Copy List with Random Pointer.java 	modified:   leetcode/Daily Question/javascript/138. Copy List with Random Pointer.js 	modified:   leetcode/Daily Question/python/138. Copy List with Random Pointer.py

--- leetcode/Daily Question/python/138. Copy List with Random Pointer.py
+++ leetcode/Daily Question/python/138. Copy List with Random Pointer.py
@@ -1,4 +1,19 @@
 """
+Автор: Дуплей Максим Игоревич - AGLA
+ORCID: https://orcid.org/0009-0007-7605-539X
+GitHub: https://github.com/QuadDarv1ne/
+ 
+Полезные ссылки:
+1. Telegram ❃ Хижина программиста Æ: https://t.me/hut_programmer_07
+2. Telegram №1 @quadd4rv1n7
+3. Telegram №2 @dupley_maxim_1999
+4. Rutube канал: https://rutube.ru/channel/4218729/
+5. Plvideo канал: https://plvideo.ru/channel/AUPv_p1r5AQJ
+6. YouTube канал: https://www.youtube.com/@it-coders
+7. ВК группа: https://vk.com/science_geeks
+"""
+
+"""
 # Definition for a Node.
 class Node:
     def __init__(self, x: int, next: 'Node' = None, random: 'Node' = None):
@@ -8,7 +23,7 @@ class Node:
 """
 
 class Solution:
-    def copyRandomList(self, head: 'Optional[Node]') -> 'Optional[Node]':
+    def copyRandomList(self, head):
         """
         Создает глубокую копию связанного списка с random указателями.
         
