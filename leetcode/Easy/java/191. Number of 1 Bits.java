767ff933285a243f4a745229cb558d92b3a52f28
QuadDarv1ne <maksimqwe42@mail.ru>
2026-02-06 13:15:37 +0300

    new file:   leetcode/Easy/cpp/191. Number of 1 Bits.cpp 	new file:   leetcode/Easy/csharp/191. Number of 1 Bits.cs 	new file:   leetcode/Easy/go/191. Number of 1 Bits.go 	new file:   leetcode/Easy/java/191. Number of 1 Bits.java 	new file:   leetcode/Easy/javascript/191. Number of 1 Bits.js 	new file:   leetcode/Easy/python/191. Number of 1 Bits.py

---
+++ leetcode/Easy/java/191. Number of 1 Bits.java
@@ -0,0 +1,34 @@
+/**
+ * Автор: Дуплей Максим Игоревич - AGLA
+ * ORCID: https://orcid.org/0009-0007-7605-539X
+ * GitHub: https://github.com/QuadDarv1ne/
+ * 
+ * Полезные ссылки:
+ * 1. Telegram ❃ Хижина программиста Æ: https://t.me/hut_programmer_07
+ * 2. Telegram №1 @quadd4rv1n7
+ * 3. Telegram №2 @dupley_maxim_1999
+ * 4. Rutube канал: https://rutube.ru/channel/4218729/
+ * 5. Plvideo канал: https://plvideo.ru/channel/AUPv_p1r5AQJ
+ * 6. YouTube канал: https://www.youtube.com/@it-coders
+ * 7. ВК группа: https://vk.com/science_geeks
+ */
+
+public class Solution {
+    /**
+     * Вычисляет количество единичных битов в числе.
+     * 
+     * @param n 32-битное беззнаковое целое число
+     * @return Количество единичных битов (вес Хэмминга)
+     */
+    public int hammingWeight(int n) {
+        int count = 0;
+        
+        while (n != 0) {
+            // Удаляем младший установленный бит
+            n &= (n - 1);
+            count++;
+        }
+        
+        return count;
+    }
+}
---
+++ leetcode/Easy/python/191. Number of 1 Bits.py
@@ -0,0 +1,34 @@
+"""
+Автор: Дуплей Максим Игоревич - AGLA
+ORCID: https://orcid.org/0009-0007-7605-539X
+GitHub: https://github.com/QuadDarv1ne/
+
+Полезные ссылки:
+1. Telegram ❃ Хижина программиста Æ: https://t.me/hut_programmer_07
+2. Telegram №1 @quadd4rv1n7
+3. Telegram №2 @dupley_maxim_1999
+4. Rutube канал: https://rutube.ru/channel/4218729/
+5. Plvideo канал: https://plvideo.ru/channel/AUPv_p1r5AQJ
+6. YouTube канал: https://www.youtube.com/@it-coders
+7. ВК группа: https://vk.com/science_geeks
+"""
+
+class Solution:
+    def hammingWeight(self, n):
+        """
+        Вычисляет количество единичных битов в числе.
+        
+        Args:
+            n: 32-битное беззнаковое целое число
+            
+        Returns:
+            Количество единичных битов (вес Хэмминга)
+        """
+        count = 0
+        
+        while n:
+            # Удаляем младший установленный бит
+            n &= (n - 1)
+            count += 1
+            
+        return count
