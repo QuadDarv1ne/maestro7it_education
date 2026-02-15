6414f8925a64b2a86bf2721f53b78cbe7941606d
QuadDarv1ne <maksimqwe42@mail.ru>
2026-02-15 13:18:12 +0300

    new file:   leetcode/Medium/cpp/287. Find the Duplicate Number.cpp 	new file:   leetcode/Medium/csharp/287. Find the Duplicate Number.cs 	new file:   leetcode/Medium/java/287. Find the Duplicate Number.java 	new file:   leetcode/Medium/javascript/287. Find the Duplicate Number.js 	new file:   leetcode/Medium/python/287. Find the Duplicate Number.py

---
+++ leetcode/Medium/cpp/287. Find the Duplicate Number.cpp
@@ -0,0 +1,40 @@
+/*
+ * LeetCode 287. Find the Duplicate Number
+ * https://leetcode.com/problems/find-the-duplicate-number/description/
+ * 
+ * Автор: Дуплей Максим Игоревич
+ * ORCID: https://orcid.org/0009-0007-7605-539X
+ * GitHub: https://github.com/QuadDarv1ne/
+ * 
+ * Полезные ссылки:
+ * 1. Telegram ❃ Хижина программиста Æ: https://t.me/hut_programmer_07
+ * 2. Telegram №1 @quadd4rv1n7
+ * 3. Telegram №2 @dupley_maxim_1999
+ * 4. Rutube канал: https://rutube.ru/channel/4218729/
+ * 5. YouTube канал: https://www.youtube.com/@it-coders
+ * 6. ВК группа: https://vk.com/science_geeks
+ */
+
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        // Фаза 1: встреча указателей
+        int slow = nums[0];
+        int fast = nums[0];
+        do {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        } while (slow != fast);
+
+        // Фаза 2: поиск входа в цикл
+        slow = nums[0];
+        while (slow != fast) {
+            slow = nums[slow];
+            fast = nums[fast];
+        }
+        return slow;
+    }
+};
---
+++ leetcode/Medium/python/287. Find the Duplicate Number.py
@@ -0,0 +1,38 @@
+'''
+https://leetcode.com/problems/find-the-duplicate-number/description/
+Автор: Дуплей Максим Игоревич - AGLA
+ORCID: https://orcid.org/0009-0007-7605-539X
+GitHub: https://github.com/QuadDarv1ne/
+
+Решение задачи "287. Find the Duplicate Number"
+
+Полезные ссылки:
+1. Telegram ❃ Хижина программиста Æ: https://t.me/hut_programmer_07
+2. Telegram №1 @quadd4rv1n7
+3. Telegram №2 @dupley_maxim_1999
+4. Rutube канал: https://rutube.ru/channel/4218729/
+5. Plvideo канал: https://plvideo.ru/channel/AUPv_p1r5AQJ
+6. YouTube канал: https://www.youtube.com/@it-coders
+7. ВК группа: https://vk.com/science_geeks
+'''
+
+# from typing import List
+
+class Solution:
+    def findDuplicate(self, nums):
+        # Фаза 1: обнаружение цикла
+        slow = nums[0]
+        fast = nums[0]
+        while True:
+            slow = nums[slow]
+            fast = nums[nums[fast]]
+            if slow == fast:
+                break
+
+        # Фаза 2: поиск входа в цикл (дубликата)
+        slow = nums[0]
+        while slow != fast:
+            slow = nums[slow]
+            fast = nums[fast]
+
+        return slow
