73e8bb14aecd9256b714c786426eab161a549b2e
QuadDarv1ne <maksimqwe42@mail.ru>
2026-03-19 08:40:09 +0300

    new file:   leetcode/Medium/cpp/3212. Count Submatrices With Equal Frequency of X and Y.cpp 	new file:   leetcode/Medium/csharp/3212. Count Submatrices With Equal Frequency of X and Y.cs 	new file:   leetcode/Medium/java/3212. Count Submatrices With Equal Frequency of X and Y.java 	new file:   leetcode/Medium/javascript/3212. Count Submatrices With Equal Frequency of X and Y.js 	new file:   leetcode/Medium/python/3212. Count Submatrices With Equal Frequency of X and Y.py

---
+++ leetcode/Medium/cpp/3212. Count Submatrices With Equal Frequency of X and Y.cpp
@@ -0,0 +1,52 @@
+/**
+ * Автор: Дуплей Максим Игоревич - AGLA
+ * ORCID: https://orcid.org/0009-0007-7605-539X
+ * GitHub: https://github.com/QuadDarv1ne/
+ * 
+ * Полезные ссылки:
+ * 1. Telegram ❃ Хижина программиста Æ: https://t.me/hut_programmer_07
+ * 2. Telegram №1 @quadd4rv1n7
+ * 3. Telegram №2 @dupley_maxim_1999
+ * 4. Rutube канал: https://rutube.ru/channel/4218729/
+ * 5. Plvideo канал: https://plvideo.ru/channel/AUPv_p1r5AQJ
+ * 6. YouTube канал: https://www.youtube.com/@it-coders
+ * 7. ВК группа: https://vk.com/science_geeks
+ */
+
+class Solution {
+public:
+    int numberOfSubmatrices(vector<vector<char>>& grid) {
+        int m = grid.size(), n = grid[0].size();
+        vector<vector<int>> prefixX(m, vector<int>(n, 0));
+        vector<vector<int>> prefixY(m, vector<int>(n, 0));
+        int count = 0;
+
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (i > 0) {
+                    prefixX[i][j] += prefixX[i-1][j];
+                    prefixY[i][j] += prefixY[i-1][j];
+                }
+                if (j > 0) {
+                    prefixX[i][j] += prefixX[i][j-1];
+                    prefixY[i][j] += prefixY[i][j-1];
+                }
+                if (i > 0 && j > 0) {
+                    prefixX[i][j] -= prefixX[i-1][j-1];
+                    prefixY[i][j] -= prefixY[i-1][j-1];
+                }
+
+                if (grid[i][j] == 'X') {
+                    prefixX[i][j] += 1;
+                } else if (grid[i][j] == 'Y') {
+                    prefixY[i][j] += 1;
+                }
+
+                if (prefixX[i][j] == prefixY[i][j] && prefixX[i][j] > 0) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+};
