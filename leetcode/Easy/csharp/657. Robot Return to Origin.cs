952723fe141cf577d865d6baa5ce037489672fab
QuadDarv1ne <maksimqwe42@mail.ru>
2026-04-05 10:32:05 +0300

    new file:   leetcode/Easy/cpp/657. Robot Return to Origin.cpp 	new file:   leetcode/Easy/csharp/657. Robot Return to Origin.cs 	new file:   leetcode/Easy/java/657. Robot Return to Origin.java 	new file:   leetcode/Easy/javascript/657. Robot Return to Origin.js 	new file:   leetcode/Easy/python/657. Robot Return to Origin.py

---
+++ leetcode/Easy/csharp/657. Robot Return to Origin.cs
@@ -0,0 +1,47 @@
+/**
+ * https://leetcode.com/problems/robot-return-to-origin/description/
+ * Автор: Дуплей Максим Игоревич - AGLA
+ * ORCID: https://orcid.org/0009-0007-7605-539X
+ * GitHub: https://github.com/QuadDarv1ne/
+ * 
+ * Решение задачи "Robot Return to Origin" на C#
+ * 
+ * Задача: Определить, возвращается ли робот в начальную точку (0,0) после выполнения всех команд.
+ * 
+ * Алгоритм:
+ * 1. Инициализируем счётчики для вертикального (x) и горизонтального (y) перемещений
+ * 2. Для каждой команды:
+ *    - 'U' (up): увеличиваем x
+ *    - 'D' (down): уменьшаем x
+ *    - 'R' (right): увеличиваем y
+ *    - 'L' (left): уменьшаем y
+ * 3. Робот вернулся в начало, если x == 0 && y == 0
+ * 
+ * Сложность: O(n) времени, O(1) памяти
+ * 
+ * Полезные ссылки:
+ * 1. Telegram ❃ Хижина программиста Æ: https://t.me/hut_programmer_07
+ * 2. Telegram №1 @quadd4rv1n7
+ * 3. Telegram №2 @dupley_maxim_1999
+ * 4. Rutube канал: https://rutube.ru/channel/4218729/
+ * 5. Plvideo канал: https://plvideo.ru/channel/AUPv_p1r5AQJ
+ * 6. YouTube канал: https://www.youtube.com/@it-coders
+ * 7. ВК группа: https://vk.com/science_geeks
+ */
+
+public class Solution {
+    public bool JudgeCircle(string moves) {
+        int x = 0, y = 0;
+        
+        foreach (char move in moves) {
+            switch (move) {
+                case 'U': x++; break;
+                case 'D': x--; break;
+                case 'R': y++; break;
+                case 'L': y--; break;
+            }
+        }
+        
+        return x == 0 && y == 0;
+    }
+}
---
+++ leetcode/Easy/python/657. Robot Return to Origin.py
@@ -0,0 +1,36 @@
+"""
+https://leetcode.com/problems/robot-return-to-origin/description/
+Автор: Дуплей Максим Игоревич - AGLA
+ORCID: https://orcid.org/0009-0007-7605-539X
+GitHub: https://github.com/QuadDarv1ne/
+
+Решение задачи "Robot Return to Origin" на Python
+
+Задача: Определить, возвращается ли робот в начальную точку (0,0) после выполнения всех команд.
+
+Алгоритм:
+1. Инициализируем счётчики для вертикального (x) и горизонтального (y) перемещений
+2. Для каждой команды:
+   - 'U' (up): увеличиваем x
+   - 'D' (down): уменьшаем x
+   - 'R' (right): увеличиваем y
+   - 'L' (left): уменьшаем y
+3. Робот вернулся в начало, если x == 0 and y == 0
+
+Сложность: O(n) времени, O(1) памяти
+
+Полезные ссылки:
+1. Telegram ❃ Хижина программиста Æ: https://t.me/hut_programmer_07
+2. Telegram №1 @quadd4rv1n7
+3. Telegram №2 @dupley_maxim_1999
+4. Rutube канал: https://rutube.ru/channel/4218729/
+5. Plvideo канал: https://plvideo.ru/channel/AUPv_p1r5AQJ
+6. YouTube канал: https://www.youtube.com/@it-coders
+7. ВК группа: https://vk.com/science_geeks
+"""
+
+class Solution:
+    def judgeCircle(self, moves):
+        x = moves.count('U') - moves.count('D')
+        y = moves.count('R') - moves.count('L')
+        return x == 0 and y == 0
