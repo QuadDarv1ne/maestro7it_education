bce28ee441bed1db1862b8f71f6803ac2a6bb5cf
QuadDarv1ne <maksimqwe42@mail.ru>
2026-04-19 10:46:19 +0300

    new file:   leetcode/Medium/cpp/1855. Maximum Distance Between a Pair of Values.cpp 	new file:   leetcode/Medium/csharp/1855. Maximum Distance Between a Pair of Values.cs 	new file:   leetcode/Medium/java/1855. Maximum Distance Between a Pair of Values.java 	new file:   leetcode/Medium/javascript/1855. Maximum Distance Between a Pair of Values.js 	new file:   leetcode/Medium/python/1855. Maximum Distance Between a Pair of Values.py

---
+++ leetcode/Medium/javascript/1855. Maximum Distance Between a Pair of Values.js
@@ -0,0 +1,38 @@
+/**
+ * Автор: Дуплей Максим Игоревич - AGLA
+ * ORCID: https://orcid.org/0009-0007-7605-539X
+ * GitHub: https://github.com/QuadDarv1ne/
+ * 
+ * Полезные ссылки:
+ * 1. Telegram ❃ Хижина программиста Æ: https://t.me/hut_programmer_07
+ * 2. Telegram №1 @quadd4rv1n7
+ * 3. Telegram №2 @dupley_maxim_1999
+ * 4. Rutube канал: https://rutube.ru/channel/4218729/
+ * 5. Plvideo канал: https://plvideo.ru/channel/AUPv_p1r5AQJ
+ * 6. YouTube канал: https://www.youtube.com/@it-coders
+ * 7. ВК группа: https://vk.com/science_geeks
+ */
+
+/**
+ * Находит максимальное расстояние между валидной парой индексов (i, j),
+ * где i <= j и nums1[i] <= nums2[j].
+ * 
+ * @param {number[]} nums1 - первый невозрастающий массив
+ * @param {number[]} nums2 - второй невозрастающий массив
+ * @return {number} максимальное расстояние (j - i) или 0
+ */
+var maxDistance = function(nums1, nums2) {
+    let i = 0, j = 0, maxDist = 0;
+    
+    while (i < nums1.length && j < nums2.length) {
+        if (nums1[i] <= nums2[j]) {
+            if (i <= j) {
+                maxDist = Math.max(maxDist, j - i);
+            }
+            j++;
+        } else {
+            i++;
+        }
+    }
+    return maxDist;
+};
