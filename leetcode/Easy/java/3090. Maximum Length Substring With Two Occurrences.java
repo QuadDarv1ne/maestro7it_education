9e71e363dbf8432f4b01714bdd48bfa65459a229
QuadDarv1ne <maksimqwe42@mail.ru>
2026-08-14 11:50:11 +0300

    new file:   leetcode/Easy/cpp/3090. Maximum Length Substring With Two Occurrences.cpp 	new file:   leetcode/Easy/csharp/3090. Maximum Length Substring With Two Occurrences.cs 	new file:   leetcode/Easy/java/3090. Maximum Length Substring With Two Occurrences.java 	new file:   leetcode/Easy/javascript/3090. Maximum Length Substring With Two Occurrences.js 	new file:   leetcode/Easy/python/3090. Maximum Length Substring With Two Occurrences.py

---
+++ leetcode/Easy/java/3090. Maximum Length Substring With Two Occurrences.java
@@ -0,0 +1,41 @@
+/**
+ * Автор: Дуплей Максим Игоревич - AGLA
+ * ORCID: https://orcid.org/0009-0007-7605-539X
+ * GitHub: https://github.com/QuadDarv1ne/
+ * 
+ * Полезные ссылки:
+ * 1. Telegram ❃ Хижина программиста Æ: https://t.me/hut_programmer_07
+ * 2. Telegram №1 @quadd4rv1n7
+ * 3. Telegram №2 @dupley_maxim_1999
+ * 4. Rutube канал: https://rutube.ru/channel/4218729/
+ * 5. Plvideo канал: https://plvideo.ru/channel/AUPv_p1r5AQJ
+ * 6. YouTube канал: https://www.youtube.com/@it-coders
+ * 7. ВК группа: https://vk.com/science_geeks
+ */
+
+class Solution {
+    /**
+     * Возвращает максимальную длину подстроки, в которой каждый символ
+     * встречается не более двух раз.
+     * Используется метод скользящего окна.
+     */
+    public int maximumLengthSubstring(String s) {
+        int[] freq = new int[26];
+        int left = 0;
+        int ans = 0;
+
+        for (int right = 0; right < s.length(); right++) {
+            char ch = s.charAt(right);
+            freq[ch - 'a']++;
+
+            while (freq[ch - 'a'] > 2) {
+                freq[s.charAt(left) - 'a']--;
+                left++;
+            }
+
+            ans = Math.max(ans, right - left + 1);
+        }
+
+        return ans;
+    }
+}
